c577590758b368daabaf45105c5aef88bc111717
Jon Wayne Parrott <thea@thea.codes>
2018-03-18 09:53:59 -0700

    Hopefully fix Travis builds

--- src/cmarkgfm/build_cmark.py
+++ src/cmarkgfm/build_cmark.py
@@ -1,6 +1,8 @@
-import os
+import distutils.ccompiler
+import distutils.dist
 import glob
 import io
+import os
 
 import cffi
 
@@ -33,13 +35,38 @@ def _get_sources(dir, exclude=set()):
 SOURCES = _get_sources(SRC_DIR, exclude=set(['main.c']))
 SOURCES.extend(_get_sources(EXTENSIONS_SRC_DIR))
 
+
+def _compiler_type():
+    """
+    Gets the compiler type from distutils. On Windows with MSVC it will be
+    "msvc". On macOS and linux it is "unix".
+
+    Borrowed from https://github.com/pyca/cryptography/blob\
+        /05b34433fccdc2fec0bb014c3668068169d769fd/src/_cffi_src/utils.py#L78
+    """
+    dist = distutils.dist.Distribution()
+    dist.parse_config_files()
+    cmd = dist.get_command_obj('build')
+    cmd.ensure_finalized()
+    compiler = distutils.ccompiler.new_compiler(compiler=cmd.compiler)
+    return compiler.compiler_type
+
+
+COMPILER_TYPE = _compiler_type()
+if COMPILER_TYPE == 'unix':
+    EXTRA_COMPILE_ARGS = ['-std=c99']
+else:
+    EXTRA_COMPILE_ARGS = None
+
+
 ffibuilder = cffi.FFI()
 ffibuilder.cdef(CMARK_DEF_H)
 ffibuilder.set_source(
     'cmarkgfm._cmark',
     CMARK_MODULE_H,
     sources=SOURCES,
-    include_dirs=[SRC_DIR, EXTENSIONS_SRC_DIR, GENERATED_SRC_DIR]
+    include_dirs=[SRC_DIR, EXTENSIONS_SRC_DIR, GENERATED_SRC_DIR],
+    extra_compile_args=EXTRA_COMPILE_ARGS
 )
 
 
