11f4d5b7dd25e05538371dd1d5dba50c69eb3273
mbarkhau <mbarkhau@gmail.com>
2020-12-10 23:12:14 +0000

    Fix CI Build (#28)
    Add sdist to CI build
    Document missing python2.7 wheels
    Bump version to 0.5.1a1

--- setup.py
+++ setup.py
@@ -25,9 +25,10 @@ class custom_build_ext(build_ext):
 
 setup(
     name='cmarkgfm',
-    version='0.5.0',
+    version='0.5.1a1',
     description="Minimal bindings to GitHub's fork of cmark",
     long_description=long_description,
+    long_description_content_type="text/x-rst",
     url='https://github.com/theacodes/cmarkgfm',
     author='The Python Packaging Authority',
     author_email='me@thea.codes, pypa-dev@googlegroups.com',
@@ -43,6 +44,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
