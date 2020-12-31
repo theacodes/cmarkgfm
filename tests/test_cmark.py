dea09d820b1bc1f8d5dd7ab835c423eed3d94423
Manuel Barkhau <mbarkhau@gmail.com>
2020-12-31 13:50:01 +0000

    fix whitespace issues in test suite

--- tests/test_cmark.py
+++ tests/test_cmark.py
@@ -5,6 +5,10 @@ import textwrap
 from cmarkgfm import cmark
 
 
+def _normalize_ws(html):
+    return textwrap.dedent(html.strip("\n")).strip("\n")
+
+
 def test_markdown_to_html():
     text = u"Hello, **world**!"
     result = cmark.markdown_to_html(text)
@@ -15,34 +19,39 @@ def test_render_html_with_extensions():
     text = u"Hello, https://pypa.io!"
     result = cmark.markdown_to_html_with_extensions(
         text, extensions=['autolink'])
-    assert result == textwrap.dedent("""\
-        <p>Hello, <a href="https://pypa.io">https://pypa.io</a>!</p>\n""")
+    expected = """
+        <p>Hello, <a href="https://pypa.io">https://pypa.io</a>!</p>
+    """
+    assert _normalize_ws(result) == _normalize_ws(expected)
 
 
 def test_github_flavored_markdown_to_html():
     text = u"Hello, https://pypa.io!"
     result = cmark.github_flavored_markdown_to_html(text)
-    assert result == textwrap.dedent("""\
-        <p>Hello, <a href="https://pypa.io">https://pypa.io</a>!</p>\n
-        """)
+    expected = """<p>Hello, <a href="https://pypa.io">https://pypa.io</a>!</p>"""
+    assert _normalize_ws(result) == _normalize_ws(expected)
 
 
 def test_github_flavored_markdown_to_html_pre_tag():
     text = u"```python\nprint('hello')\n```"
     result = cmark.github_flavored_markdown_to_html(text)
-    assert result == textwrap.dedent("""\
+    expected = """
         <pre><code class="language-python">print('hello')
         </code></pre>
-        """)
+    """
+    assert _normalize_ws(result) == _normalize_ws(expected)
 
     
 def test_github_flavored_markdown_to_html_tasklist():
     text = u"- [X] Task 1 Done\n- [ ] Task 2 Incomplete"
     result = cmark.github_flavored_markdown_to_html(text)
-    assert result == textwrap.dedent("""\
-        <ul>\n<li><input type="checkbox" disabled="" /> Task 1 Done</li>\n
-        <li><input type="checkbox" disabled="" /> Task 2 Incomplete</li>\n</ul>\n
-        """)
+    expected = """
+        <ul>
+        <li><input type="checkbox" disabled="" /> Task 1 Done</li>
+        <li><input type="checkbox" disabled="" /> Task 2 Incomplete</li>
+        </ul>
+    """
+    assert _normalize_ws(result) == _normalize_ws(expected)
 
 
 def test_parse_document():
